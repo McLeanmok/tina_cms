cd080efcd2c342b3db31239a9f0fa448d26cd4c4
McLeanmok <165439265+McLeanmok@users.noreply.github.com>
2026-07-14 14:37:40 -0700

    Theme is live

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+import purgecss from 'astro-purgecss';
+export default defineConfig({
+    // Add purgecss support to Astro. This will not output anything that is not used in your final build.
+  integrations: [purgecss({
+    fontFace: true,
+   variables: true,
+  }),],
+});
