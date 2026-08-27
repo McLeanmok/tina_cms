06d1a40b32045ae25c4d09e5d2f5ee2013e160f0
McLeanmok <165439265+McLeanmok@users.noreply.github.com>
2026-08-27 12:24:23 -0700

    Installed plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 import tina from '@tinacms/astro/integration';
 import { tinaAdminDevRedirect } from '@tinacms/astro/vite';
 
 import mdx from '@astrojs/mdx';
-
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
